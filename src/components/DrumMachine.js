ab6634ae0c8a0a4bdf5f4e05a2e9cacf3142ff7a
Alberto <aaayza@gmail.com>
2022-05-28 20:11:06 -0300

    Firsts styles

--- src/components/DrumMachine.js
+++ src/components/DrumMachine.js
@@ -2,21 +2,24 @@ import React from "react";
 
 const DrumMachine = () => {
   return (
-    <div id="drum-machine">
+    <>
       <label className="switch">
         <input type="checkbox" />
         <span className="slider round"></span>
       </label>
-      <button className="drum-pad"></button>
-      <button className="drum-pad"></button>
-      <button className="drum-pad"></button>
-      <button className="drum-pad"></button>
-      <button className="drum-pad"></button>
-      <button className="drum-pad"></button>
-      <button className="drum-pad"></button>
-      <button className="drum-pad"></button>
-      <button className="drum-pad"></button>
+    <div id="drum-machine">
+      <button className="drum-pad">Q</button>
+      <button className="drum-pad">W</button>
+      <button className="drum-pad">E</button>
+      <button className="drum-pad">A</button>
+      <button className="drum-pad">S</button>
+      <button className="drum-pad">D</button>
+      <button className="drum-pad">Z</button>
+      <button className="drum-pad">X</button>
+      <button className="drum-pad">C</button>
+      <label id="display">asdasd</label>
     </div>
+    </>
   );
 };
 
